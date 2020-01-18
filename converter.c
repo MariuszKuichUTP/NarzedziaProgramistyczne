776f100091bbee4c52aeac991644acface369a00
Mariusz <markui000@utp.edu.pl>
2020-01-18 21:13:52 +0100

    update converter.c (now it checks correctness of entered data)

--- converter.c
+++ converter.c
@@ -4,8 +4,14 @@
 int main()
 {
     float number;
-    printf("Enter number of gigabytes [GB] to convert: ");
-    scanf("%f", &number);
+    do
+    {
+        printf("Enter number of gigabytes [GB] to convert: ");
+        scanf("%f", &number);
+        if(number < 0) printf("Enter correct (non-negative) value!\n");
+    }
+    while(number < 0);
+
     printf("\n%.2f GB = %.2f MB = %.2f KB = %.2f B = %.2f b\n", number, number*1000, number*1000000, number*1000000000, number*8000000000);
     return 0;
 }
