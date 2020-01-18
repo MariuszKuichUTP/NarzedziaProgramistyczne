342beb83ab527508526bb4e9756624e4b7c1e0ac
Mariusz <markui000@utp.edu.pl>
2020-01-18 21:29:58 +0100

    update converter.c (now it converts not only GB -> MB -> ... but also GiB -> MiB -> ...)

--- converter.c
+++ converter.c
@@ -6,12 +6,13 @@ int main()
     float number;
     do
     {
-        printf("Enter number of gigabytes [GB] to convert: ");
+        printf("Enter number of gigabytes [GB] and gibibytes [GiB] to convert: ");
         scanf("%f", &number);
         if(number < 0) printf("Enter correct (non-negative) value!\n");
     }
     while(number < 0);
 
-    printf("\n%.2f GB = %.2f MB = %.2f KB = %.2f B = %.2f b\n", number, number*1000, number*1000000, number*1000000000, number*8000000000);
+    printf("\n%.2f GB = %.2f MB = %.2f KB = %.2f B = %.2f b\n", number, number*1000, number*1000*1000, number*1000*1000*1000, number*1000*1000*1000*8);
+    printf("\n%.2f GiB = %.2f MiB = %.2f KiB = %.2f B = %.2f b\n", number, number*1024, number*1024*1024, number*1024*1024*1024, number*1024*1024*1024*8);
     return 0;
 }
