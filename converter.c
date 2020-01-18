41dd2be5eb7e9ceab8d95579fbe9bb154e2371fd
Mariusz <markui000@utp.edu.pl>
2020-01-18 18:53:19 +0100

    add simple default program (base for programs located on different branches)

---
+++ converter.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    float number;
+    printf("Enter number of kilometers to convert: ");
+    scanf("%f", &number);
+    printf("\n%.2f km = %.2f m = %.2f dm = %.2f cm = %.2f mm\n", number, number*1000, number*10000, number*100000, number*1000000);
+    return 0;
+}
